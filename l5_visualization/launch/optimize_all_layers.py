09016de0124b1d1343bcb13fd2792e970e5b8669
leonid <erlygin.la@phystech.edu>
2022-09-09 14:01:34 +0000

    opt all layers

---
+++ l5_visualization/launch/optimize_all_layers.py
@@ -0,0 +1,54 @@
+import hydra
+from pathlib import Path
+import hydra
+from tensorflow import keras
+from optimizators import ActivationOptimizer
+import cv2
+import numpy as np
+import tensorflow as tf
+import sys
+sys.path.append('/workspaces/lectures')
+
+
+@hydra.main(config_path="../configs/hydra", config_name=Path(__file__).stem + "_config")
+def run(cfg):
+    model = keras.models.load_model(cfg.model_path)
+    # define optimizator
+
+    for layer in model.layers:
+        if 'conv2d' not in layer.name:
+            continue
+        for filter_index in range(layer.filters):
+            ao = ActivationOptimizer(
+                model=model,
+                layer_name=conv_layer,
+                activation_index=(slice(None), slice(None), filter_index),
+                steps=cfg.steps,
+                step_size=cfg.step_size,
+                reg_coef=cfg.reg_coef,
+            )
+            for i in range(cfg.n_images_per_filter):
+                random_image = tf.random.uniform(
+                    [32, 32, 3],
+                    minval=0,
+                    maxval=None,
+                    dtype=tf.dtypes.float32,
+                    seed=None,
+                    name=None,
+                )
+                loss, image_raw = ao(random_image)
+
+                image = 255 * (image_raw)
+                image = tf.cast(image, tf.uint8)
+                image = np.array(image)
+                # resize
+                image = cv2.resize(image, cfg.resize_image_to)
+
+                cv2.imwrite('f.png', cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    for conv_layer in conv_layer_names:
+        
+
+if __name__ == "__main__":
+    # load model
+    
+    run()
